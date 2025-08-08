aac9dd857f560d76dca4a9254c3faaac5a75f3eb
yashr775 <rajy29629@gmail.com>
2025-08-08 13:34:16 +0530

    Initialised a fastapi backend

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <div className="bg-blue-600  h-15 flex justify-between">
+        <div className="bg-blue-900  h-15 flex justify-between">
             <div className="pt-2 pl-2 text-white text-3xl font-bold">PyDo</div>
             <div className="pt-2 pr-8">
                 <button className=" h-10 p-2 pr-2 bg-blue-500 text-lg text-white font-bold hover:bg-blue-600 rounded-lg cursor-pointer">
